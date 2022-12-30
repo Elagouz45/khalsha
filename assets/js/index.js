664c46673917777dce41a566d6ab386034fd307c
El-Iman <eman94682@gmail.com>
2022-12-30 19:58:21 +0200

    30-12-2022

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,64 @@
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel();
+ 
+
+$(document).ready(function () {
+
+  //my-account page
+  $(".sidebar ul li").click(function () {
+    $(".sidebar ul li").removeClass("active");
+    $(this).addClass("active");
   });
+
+
+  var linksList = Array.from(document.querySelectorAll('.sidebar ul li'));
+  var contentList = Array.from(document.querySelectorAll('.my-account  .content'));
+  linksList.forEach((element) => {
+    element.addEventListener('click', () => {
+      let href = element.getAttribute('data-target')?.toString();
+
+      contentList.forEach((content) => {
+        if (content.classList.contains(href)) {
+          content.classList.add('d-block');
+          content.classList.remove('d-none');
+        } else {
+          content.classList.add('d-none');
+          content.classList.remove('d-block');
+        }
+      });
+
+    });
+  });
+
+
+
+
+
+//register page
+  $(".toggle-active-btn").click(function () {
+    $(".toggle-active-btn").removeClass("active");
+    $(this).addClass("active");
+  });
+
+// using-way
+  // $(".using-way .owl-carousel").owlCarousel({
+  //   animateIn: 'fadeIn',
+  //   animateOut: 'fadeOut',
+  //   items: 1,
+  //   margin: 0,
+  //   padding: 0,
+  //   stagePadding: 0,
+  //   smartSpeed: 800,
+  //   navs: true,
+  //   loop: true,
+  //   autoplay: true,
+  //   autoPlaySpeed: 5000,
+  //   autoPlayTimeout: 5000,
+  //   autoplayHoverPause: true
+  // })
+
+ 
+
+  
+
+});
+
+
