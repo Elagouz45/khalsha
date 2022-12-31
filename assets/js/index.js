a2a392914cce78f2405234b6c7bcd145f97531b2
Elagouz45 <mahmoudelagouz21@gmail.com>
2022-12-31 22:05:55 +0200

    merge

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,22 @@
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel();
-  });
+$(document).ready(function () {
+    $(".home-header-slider").owlCarousel({
+        autoplay: true,
+        smartSpeed: 800,
+        dots: true,
+        mouseDrag: true,
+        margin: 10,
+        navigation: true,
+        slideBy: 1,
+        items: 1,
+    }) ;
+    $(".about-us-slider").owlCarousel({
+        autoplay: true,
+        smartSpeed: 800,
+        dots: true,
+        mouseDrag: true,
+        margin: 10,
+        navigation: true,
+        slideBy: 1,
+        items: 1,
+    })
+});
